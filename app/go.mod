738fde9cdef4ef062b59d787c8c65aeab57c9a2a
S-H <hirais@ace-g.com>
2021-06-13 20:53:44 +0900

    docker file修正、json api追加

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,7 @@ module app
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.3.0 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -3,10 +3,22 @@ package main
 import (
     "net/http"
     "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
+    "app/handler"
 )
 
 func main() {
     e := echo.New()
+
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    e.POST("/login", handler.Login())
+    r := e.Group("/restricted")
+    r.Use(middleware.JWT([]byte("secret")))
+    r.GET("/welcome", handler.Restricted())
+
+
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello galaxy!")
     })
